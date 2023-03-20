ab6a2ddb64781b6efb08e3a96eed2f5264f08943
Ogbu Isreal <ogbuisreal22@gmail.com>
2023-03-20 21:39:19 +0100

    DEPLOYING APP

--- solarappbackend/settings.py
+++ solarappbackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yf(4r=kwcia^)23^rf3kf!oq=#4zr)1l=^f+s2tfx01n=7z=ze'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "solarapp.pythonanywhere.com"]
 
 # Application definition
 
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
